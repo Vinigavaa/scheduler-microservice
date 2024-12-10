aaa105c9b7c1c45e62798cf5e7c2e6b184333545
Zer0nights <137710927+Zer0nights@users.noreply.github.com>
2024-12-10 19:30:49 -0300

    feat: Create DataLoader

--- apims/src/main/java/com/microsservice/apims/config/DataLoader.java
+++ apims/src/main/java/com/microsservice/apims/config/DataLoader.java
@@ -1,4 +1,33 @@
 package com.microsservice.apims.config;
 
-public class DataLoader {
+import com.microsservice.apims.entity.Channel;
+import com.microsservice.apims.entity.Status;
+import com.microsservice.apims.repository.ChannelRepository;
+import com.microsservice.apims.repository.StatusRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class DataLoader implements CommandLineRunner {
+
+    private final ChannelRepository channelRepository;
+    private final StatusRepository statusRepository;
+
+    public DataLoader(ChannelRepository channelRepository, StatusRepository statusRepository) {
+        this.channelRepository = channelRepository;
+        this.statusRepository = statusRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Arrays.stream(Channel.Values.values())
+                .map(Channel.Values::toChannel)
+                .forEach(channelRepository::save);
+
+        Arrays.stream(Status.Values.values())
+                .map(Status.Values::toStatus)
+                .forEach(statusRepository::save);
+    }
 }
