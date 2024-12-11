46fa04b3ae82633453f2f0c19fa20cda3b92af09
Vinigavaa <vinigava14@gmail.com>
2024-12-11 15:03:43 -0300

    correcting errors

--- apims/src/main/java/com/microsservice/apims/ApimsApplication.java
+++ apims/src/main/java/com/microsservice/apims/ApimsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//iniciar
 public class ApimsApplication {
 
 	public static void main(String[] args) {
